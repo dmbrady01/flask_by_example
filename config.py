e2b164d3397c241e472d2e3be8ed89769ad6cb95
Daniel Mahefasoa Brady <dmbrady01@gmail.com>
2020-03-10 19:07:09 -0400

    Added config file

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+basedir: str = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG: bool = False
+    DEVELOPMENT: bool = False
+    TESTING: bool = False
+    CSRF_ENABLED: bool = True
+    SECRET_KEY: str = 'this-really-needs-to-be-changed'
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
