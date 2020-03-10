0ebb9361f6c179815b2c77d18375192611041736
Daniel Mahefasoa Brady <dmbrady01@gmail.com>
2020-03-10 19:14:21 -0400

    Fixed env setting for app

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_envvar('APP_SETTINGS')
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello() -> str:
@@ -13,3 +14,4 @@ def hello_name(name: str) -> str:
 
 if __name__ == '__main__':
     app.run()
+    print(os.environ['APP_SETTINGS'])
