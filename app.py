fc483630ce70b9b05713f69b3e21b1f27fff24c5
Daniel Mahefasoa Brady <dmbrady01@gmail.com>
2020-03-11 15:14:07 -0400

    Added migrations

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-app.config['SQLALCHEMY_TRACK_MODICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from models import Result
 
